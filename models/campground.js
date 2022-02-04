8002969ead91f05b19419f4638c87742eb050d72
daniellim922 <daniellim922@gmail.com>
2022-02-05 02:52:23 +0800

    patched a reference error

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const review = require('./review');
-const Schema = mongoose.Schema;
+const Review = require('./review');
+const { Schema } = mongoose;
 
 const CampgroundSchema = new Schema({
 	title: String,
